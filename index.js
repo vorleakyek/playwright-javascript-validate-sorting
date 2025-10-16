349eac77472e78db48a9c66615ee31c6d858e660
Vorleak Yek <vorleakyek@gmail.com>
2025-10-15 22:18:38 -0700

    Refactor the sort function and retrieve article dates on the current page.

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 // EDIT THIS FILE TO COMPLETE ASSIGNMENT QUESTION 1
+const { get } = require("http");
 const { chromium } = require("playwright");
 
-function compareDates(date1, date2) {
-  return new Date(date1) - new Date(date2);
-}
+// get all articles
+const getArticleDates = async () => {
+  const articleTimes = await page
+    .locator("td.subtext span.age")
+    .evaluateAll((elements) => elements.map((el) => el.getAttribute("title")));
+  const extractedArticleDates = articleTimes.map((time) => time.split(" ")[1]);
+  return extractedArticleDates;
+};
+
+// validate if the set of articles on the current page is sorted from newest to oldest
+const isSorted = (articleDates, isNextPage) => {
+  for (let i = 0; i < articleDates.length - 2; i++) {
+    if (articleDates[i] - articleDates[i + 1] < 0) {
+      isNextPage = false;
+      return false;
+    }
+  }
+
+  return true;
+};
 
 async function sortHackerNewsArticles() {
   // launch browser
@@ -15,26 +33,68 @@ async function sortHackerNewsArticles() {
   await page.goto("https://news.ycombinator.com/newest");
   await page.waitForLoadState("networkidle");
 
-  // get all articles
-  const articleTimes = await page
-    .locator("td.subtext span.age")
-    .evaluateAll((elements) => elements.map((el) => el.getAttribute("title")));
+  let totalArticles = 0;
+  const maxArticles = 100;
+  isNextPage = true;
 
-  // validate if that the first 30 articles are sorted from newest to oldest
-  for (let i = 0; i < articleTimes.length - 1; i++) {
-    if (
-      compareDates(
-        parseInt(articleTimes[i][1]),
-        parseInt(articleTimes[i + 1][1])
-      ) > 0
-    ) {
-      console.log(articleTimes[i], articleTimes[i + 1]);
+  // get article times
+
+  while (isNextPage && totalArticles < maxArticles) {
+    const articleDates = await getArticleDates();
+    if (!isSorted(articleDates, totalArticles, maxArticles, isNextPage)) {
       console.log("Articles are not sorted from newest to oldest properly.");
-    } else {
-      console.log("Articles are sorted from newest to oldest properly.");
       return;
     }
+    totalArticles += articleDates.length;
+    const moreLink = page.locator('a.morelink:has-text("More")');
+    await moreLink.click();
   }
+
+  // validate if that the first 30 articles are sorted from newest to oldest
+  let isNextSet = true;
+  // for (let i = 0; i < articleTimes.length - 1; i++) {
+  //   if (
+  //     compareDates(
+  //       parseInt(articleTimes[i][1]),
+  //       parseInt(articleTimes[i + 1][1])
+  //     ) > 0
+  //   ) {
+  //     isNextSet = false;
+  //     console.log(articleTimes[i], articleTimes[i + 1]);
+  //     console.log("Articles are not sorted from newest to oldest properly.");
+  //     return;
+  //   }
+  //   totalArticles++;
+  // }
+
+  // if (isNextSet) {
+  //   const moreLink = page.locator('a.morelink:has-text("More")');
+  //   await moreLink.click();
+  //   await page.waitForLoadState("networkidle");
+
+  //   const newArticleTimes = await page
+  //     .locator("td.subtext span.age")
+  //     .evaluateAll((elements) =>
+  //       elements.map((el) => el.getAttribute("title"))
+  //     );
+
+  //   for (let i = 0; i < newArticleTimes.length - 1; i++) {
+  //     if (totalArticles < 100) {
+  //       if (
+  //         compareDates(
+  //           parseInt(newArticleTimes[i][1]),
+  //           parseInt(newArticleTimes[i + 1][1])
+  //         ) > 0
+  //       ) {
+  //         console.log(newArticleTimes[i], newArticleTimes[i + 1]);
+  //         console.log("Articles are not sorted from newest to oldest properly.");
+  //       }
+  //     }
+  //     totalArticles++;
+  //   }
+  // }
+
+  //
 }
 
 (async () => {
